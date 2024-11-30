86f568ca8a47d7479f0ea224700d8ebfdadfd1e4
bushraabir <muzahidulislamabir123@gmail.com>
2024-11-30 13:03:04 +0600

    port added

--- backend/app.py
+++ backend/app.py
@@ -363,4 +363,5 @@ def profile():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Use PORT from the environment or default to 5000
+    app.run(host="0.0.0.0", port=port)
